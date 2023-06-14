4226957e36ea755a5d193b74ad72a2f4465e91bb
Ahmed Ayman <Ahmed.Ayman@corplease.com.eg>
2023-06-14 15:06:10 +0300

    End of section 2

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+
+namespace Infrastructure.Data;
+
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions options) 
+    : base(options)
+    {
+    }
+
+    // DbSets for Tables 
+    public DbSet<Product> Products { get; set; }
+}
